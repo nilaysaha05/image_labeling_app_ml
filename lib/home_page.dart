e7ccaa9b9317613c69f2c1843fb9d6b83c403bf8
nilaysaha05 <sahanilay.05@gmail.com>
2023-03-28 00:17:18 +0530

    final commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
 import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,6 +13,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late ImagePicker _imagePicker;
   File? _image;
+  String result = "results will be shown here";
+  dynamic imageLabeler;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _imagePicker = ImagePicker();
+    final ImageLabelerOptions options =
+        ImageLabelerOptions(confidenceThreshold: 0.5);
+    imageLabeler = ImageLabeler(options: options);
+  }
 
   @override
   void dispose() {
@@ -19,72 +32,104 @@ class _HomePageState extends State<HomePage> {
     super.dispose();
   }
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _imagePicker = ImagePicker();
-  }
-
-
   _imageFromGallery() async {
-    XFile? pickedFile = await _imagePicker.pickImage(source:ImageSource.gallery);
-    if(pickedFile!=null)
-      {
-        setState(() {
-          _image = File(pickedFile.path);
-        });
-      }
-
+    XFile? pickedFile =
+        await _imagePicker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+        doImageLabeling();
+      });
+    }
   }
 
   _imageFromCamera() async {
-    XFile? pickedFile = await _imagePicker.pickImage(source:ImageSource.camera);
+    XFile? pickedFile =
+        await _imagePicker.pickImage(source: ImageSource.camera);
+    _image = File(pickedFile!.path);
+    setState(() {
+      _image;
+      doImageLabeling();
+    });
+  }
 
-      setState(() {
-        _image = File(pickedFile!.path);
-      });
-
-
+  doImageLabeling() async {
+    InputImage inputImage = InputImage.fromFile(_image!!);
+    final List<ImageLabel> labels = await imageLabeler.processImage(inputImage);
+    result = "";
+    for (ImageLabel label in labels) {
+      final String text = label.label;
+      final int index = label.index;
+      final double confidence = label.confidence;
+      result += "$text ${confidence.toStringAsFixed(2)}\n";
+    }
+    setState(() {
+      result;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text("Image Labeler"),
+        ),
+      ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 100,
-            ),
-            Center(
-              child: Container(
-                height: 250,
-                width: 250,
-                color: Colors.grey,
-                child: _image ==null ? const Icon(
-                  Icons.image,
-                  size: 100,
-                ) : Image.file(_image!),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 45,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: _imageFromGallery,
-              child: const Text("Gallery"),
-            ),
-            ElevatedButton(
-              onPressed: _imageFromCamera,
-              child: const Text("Camera"),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text('Results will show here'),
-          ],
+              Center(
+                child: Container(
+                  height: 350,
+                  width: 250,
+                  color: Colors.grey,
+                  child: _image == null
+                      ? const Icon(
+                          Icons.image,
+                          size: 100,
+                        )
+                      : Image.file(_image!),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 45,
+                child: ElevatedButton(
+                  onPressed: _imageFromGallery,
+                  child: const Text("choose from Gallery"),
+                ),
+              ),
+              const SizedBox(height: 20.0,),
+              SizedBox(
+                width: double.infinity,
+                height: 45,
+                child: ElevatedButton(
+                  onPressed: _imageFromCamera,
+                  child: const Text("capture Image"),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                result,
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
